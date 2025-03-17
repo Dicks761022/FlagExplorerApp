8ddf31abf351984383d7854a816de4baf8a340e1
Seraj <serajdicks@gmail.com>
2025-03-17 04:20:01 +0200

    header styling

--- frontend/flagExplorerFrontend/src/components/Header.jsx
+++ frontend/flagExplorerFrontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header({ setSearchFlag, setSelectedRegion }) {
     <div className='home-header-container'>
     <div className="home-header">
     <div className="header-text">
-      <h1>Welcome to Flag Explorer</h1>
+      <h1 className='my-heading'>Welcome to Flag Explorer</h1>
       <p>Discover the world, one flag at a time. Dive into a colorful journey and learn more about countries from every corner of the globe.Whether you're curious about the population, the name, or the flag of a country, our easy-to-use platform lets you search, filter, and explore. Ready to explore?
          Start by searching for a flag or selecting a region, and uncover the fascinating details behind each nation!</p>
       
@@ -18,7 +18,7 @@ function Header({ setSearchFlag, setSelectedRegion }) {
       </div>
     </div>
 
-    <div className='map'></div>
+    
     </div>
     </div>
   );
